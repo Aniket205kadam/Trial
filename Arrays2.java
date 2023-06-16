d3138ea3b11ffb10332dc3993289f6de58bed9cd
Aniket205kadam <aniketrkadam205@gmail.com>
2023-06-16 21:12:49 +0530

    Use a Enhanced loop

--- Arrays2.java
+++ Arrays2.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Arrays2 {
     public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        //Input size of array
+        System.out.println("Enter size of array: ");
+        int size = sc.nextInt();
         //create a array
-        int nums[] = {1, 2, 5, 7, 8};
+        int nums[] = new int[size];
+        System.out.println("Enter a elements: ");
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = sc.nextInt();
+        }
         Sum obj = new Sum();
         int result = obj.sumOfArray(nums);
         System.out.println("Sum of array is: " + result);
@@ -11,8 +21,9 @@ public class Arrays2 {
 class Sum {
     public int sumOfArray(int nums[]) {
         int result = 0;
-        for (int i = 0; i < nums.length; i++) {
-            result += nums[i];
+        //Enhanced loop
+        for (int n : nums) {
+            result += n;
         }
         return result;
     }
